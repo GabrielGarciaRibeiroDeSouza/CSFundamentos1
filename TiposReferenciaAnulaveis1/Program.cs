198e2bfe3d0e368dda6cffabd6df4dbb9f20ef52
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2023-10-11 16:21:12 -0300

    tipo de referêcia anulável

---
+++ TiposReferenciaAnulaveis1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("## Tipos de referêcia anulável\n");
+
+string? nome = null;
+ // O '?.' verifica se a variavel nome é null, se não for null ele executa o metodo 'ToUpper()'
+Console.WriteLine(nome?.ToUpper());
+
+Console.ReadKey();
